368729c53c90932e6c58661835538d437e52af04
Ralf Wissing <ralf.wissing@posteo.de>
2023-10-19 15:10:30 +0200

    Initial commit.

---
+++ src/shoppinglist_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { shoppinglist_backend } from "../../declarations/shoppinglist_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await shoppinglist_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
