3175bbf89af38a4d74254f690437db4dfd9bfe63
Ralf Wissing <ralf.wissing@googlemail.com>
2023-10-20 13:54:53 +0200

    Alles funktioniert

--- src/shoppinglist_backend/main.mo
+++ src/shoppinglist_backend/main.mo
@@ -1,5 +1,23 @@
+import Nat "mo:base/Nat";
+import Text "mo:base/Text";
+import Iter "mo:base/Iter";
+import Buffer "mo:base/Buffer";
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+
+  let shoppingItems = Buffer.Buffer<Text>(3);
+
+
+  public query func getShoppingItems () : async [Text] {
+    return Iter.toArray(shoppingItems.vals());
   };
+
+  public func addItemToList (anItem : Text) : async () {
+    shoppingItems.add(anItem);
+  };
+
+  public func removeItemFromList (item : Nat) : async () {
+    let x = shoppingItems.remove(item);
+  }
+  
 };
--- src/shoppinglist_frontend/src/index.js
+++ src/shoppinglist_frontend/src/index.js
@@ -1,19 +1,96 @@
 import { shoppinglist_backend } from "../../declarations/shoppinglist_backend";
 
+
+const getShoppingList = async () => {
+  let shoppingList = await shoppinglist_backend.getShoppingItems();
+
+  document.getElementById("itemsToBuy").innerHTML="";
+
+  if (shoppingList.length > 0 ){ 
+    
+     for (let i = 0; i < shoppingList.length; i++){
+      appendItemToList(i,shoppingList[i])
+
+    }
+    
+  };
+};
+
+
+
+const appendItemToList = (id,item) => {
+  
+
+  let newItem = document.createElement("button");
+  newItem.textContent = item;
+  newItem.classList.add('button','is-rounded','mr-2','mt-2');
+  newItem.addEventListener("click", ()=> {
+    
+    removeFromList(id);
+  });
+
+  document.getElementById("itemsToBuy").append(newItem);
+
+
+}
+
+
+const removeFromList = async (itemToRemove) => {
+
+  isLoading();
+
+  await shoppinglist_backend.removeItemFromList(itemToRemove);
+  await getShoppingList();
+
+  finishedLoading();
+
+};
+
+
+
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+  const item = document.getElementById("item").value;
 
-  button.setAttribute("disabled", true);
+  isLoading();
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await shoppinglist_backend.greet(name);
+  await shoppinglist_backend.addItemToList(item);
+  getShoppingList();
 
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
+  finishedLoading();
+  
+  document.getElementById("item").value="";
 
   return false;
 });
+
+
+const isLoading = () => {
+
+  const loader= document.createElement("progress");
+  loader.setAttribute('id','loader');
+  loader.classList.add('progress','is-medium','is-danger');
+
+
+  const button = document.getElementById("submitButton");
+  const textfield = document.getElementById("itemsToBuy");
+
+  document.getElementById("isLoadingAnimation").appendChild(loader);
+
+  textfield.setAttribute("disabled", true);
+  button.setAttribute("disabled", true);
+
+};
+
+const finishedLoading = () => {
+  document.getElementById("isLoadingAnimation").innerHTML="";
+  
+  const button = document.getElementById("submitButton");
+  const textfield = document.getElementById("itemsToBuy");
+  document.getElementById("isLoadingAnimation").style.visibility = "hidden";
+  textfield.removeAttribute("disabled");
+  button.removeAttribute("disabled");
+}
+
+await getShoppingList();
